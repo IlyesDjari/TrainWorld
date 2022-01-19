9c76d71f2bed126a73e68291abc198ea51b80177
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-01-19 15:30:12 +0100

    Popup is shown when app is started. Close button works

--- Web-APP/src/index.js
+++ Web-APP/src/index.js
@@ -1,5 +1,13 @@
 "use strict"
 
 window.onload = function () {
+    showWelcomePopup();
+}
 
+function showWelcomePopup() {
+    const popup = document.getElementById("welcomePopup");
+    popup.style.display = "block";
+
+    const exitPopup = document.getElementById("welcomeClose");
+    exitPopup.addEventListener("click", () => popup.style.display = "none");
 }
