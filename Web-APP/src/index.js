202aa771895119eed39d7a395516a397affe6d47
IlyesDjari <ilyes.djari@student.ehb.be>
2022-01-19 13:37:01 +0100

    Added the landscape alert on index.js

--- Web-APP/src/index.js
+++ Web-APP/src/index.js
@@ -3,3 +3,10 @@
 window.onload = function () {
 
 }
+
+setInterval(() => {
+    console.log("Working");
+    if (screen.availHeight > screen.availWidth) {
+        console.log("Please use Landscape!");
+    }
+}, 1000);
