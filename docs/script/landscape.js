e896df9af268f347a350522738fe4316cdbf2829
IlyesDjari <ilyes.djari@student.ehb.be>
2022-01-19 12:25:52 +0100

    WIP Checking if the device is in landscape mode

---
+++ docs/script/landscape.js
@@ -0,0 +1,10 @@
+"use strict"
+
+
+
+setInterval(() => {
+    console.log("Working");
+    if (screen.availHeight > screen.availWidth) {
+        console.log("Please use Landscape!");
+    }
+}, 1000);
