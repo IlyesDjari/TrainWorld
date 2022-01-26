7e7ea4e38ee099aa1f34bbdf1b430d0f18dd7c08
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-01-26 11:21:27 +0100

    Update sw.js

---
+++ Web-APP/docs/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+    console.log('service worker has been installed');
+});
+
+// activate event
+self.addEventListener('activate', e => {
+    console.log('service worker has been activated');
+});
